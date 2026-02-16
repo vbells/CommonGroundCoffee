12bc9b4b97da3f5ec84ae5afddfdf6a7cb556797
Victor Martinez Macias <vmartinezmacias@my.dom.edu>
2026-02-16 14:44:32 -0600

    Creadeted products view

--- CommonGroundCoffee/Program.cs
+++ CommonGroundCoffee/Program.cs
@@ -1,6 +1,7 @@
 using BusinessLogicLayer;
 using DataAccessLayer.Data;
 using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;  // Make sure you have this
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,14 +9,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+// Database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ShopConnectionString")));
 
+// Identity
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
 })
 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// --- Add these lines for Dependency Injection ---
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
@@ -30,9 +37,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();   // IMPORTANT
+app.UseAuthentication();
 app.UseAuthorization();
 
+// Controller route
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
