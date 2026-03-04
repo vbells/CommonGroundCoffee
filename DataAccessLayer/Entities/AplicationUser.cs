7c836381baf3db468f8441538c3b2124943572e1
Victor Martinez Macias <vmartinezmacias@my.dom.edu>
2026-03-03 18:49:04 -0600

    Updated home page

--- DataAccessLayer/Entities/AplicationUser.cs
+++ DataAccessLayer/Entities/AplicationUser.cs
@@ -1,20 +1,36 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 
 
 namespace DataAccessLayer.Entities
 {
         public class ApplicationUser : IdentityUser
         {
-            // Optional: extra fields for your app
-            public string? FirstName { get; set; }
-            public string? LastName { get; set; }
+        public class InputModel
+        {
+            [Required]
+            public string FirstName { get; set; }
 
-            // Optional: link to your existing Customers table
-            public int? CustomerID { get; set; }
+            [Required]
+            public string LastName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Compare("Password")]
+            public string ConfirmPassword { get; set; }
         }
+
+    }
 }
