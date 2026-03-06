e43143608127cb89152af61f1e540b0dfeec23f9
vbellh <vbellohernandez@my.dom.edu>
2026-03-06 14:15:30 -0600

    Enabled Add to Cart Button functionality

--- CommonGroundCoffee/Controllers/CartController.cs
+++ CommonGroundCoffee/Controllers/CartController.cs
@@ -18,7 +18,7 @@ namespace CommonGroundCoffee.Controllers
             return View(cart);
         }
 
-        public async Task<IActionResult> Add(int productId, int quantity = 1)
+        public IActionResult Add(int productId, int quantity = 1)
         {
             _cartService.AddToCart(productId, quantity);
             return RedirectToAction("Index");
